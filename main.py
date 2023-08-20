9478c41ae02c6748fd8ed99829df0cacf4b8e8bc
Alexander Panferov <alexandr.n.panferov@gmail.com>
2023-08-20 21:14:14 +0700

    add user interaction code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.connector import Connector
-
+from src.sort_classes import Vacancy
 
 if __name__ == '__main__':
     print()
@@ -30,3 +30,34 @@ if __name__ == '__main__':
             print("Cпециальность не найдена")
             quit()
         vacancies = connect.select_HH()
+
+    elif user_choise == "2":
+        try:
+            connect.connectSJ()
+        except IndexError:
+            print("Cпециальность не найдена")
+            quit()
+        vacancies = connect.select_SJ()
+    else:
+        print("Спасибо, что воспользовались нашим приложением")
+        quit()
+
+    for vacancy in vacancies:
+        vacancy_class = Vacancy(vacancy['employer'], vacancy['name'], vacancy['url'], vacancy['requirement'],
+                                vacancy['salary_from'], vacancy['salary_to'])
+        print(vacancy_class)
+    print(f"Всего найдено вакансий {len(vacancies)}")
+    user_sort = input("\nХотите отсортировать вакансии по зарплате(от большего к меньшему Y/N? ").lower()
+    if user_sort == "y":
+        for sort in Vacancy.sorting(vacancies):
+            print(sort)
+        try:
+            user_top = int(input("""\nХотите вывести список топ вакансий? 
+    (Введите количество): """))
+            if user_top:
+                for top in Vacancy.get_top(vacancies, user_top):
+                    print(top)
+        except ValueError:
+            print("Спасибо, что воспользовались нашим приложением")
+    else:
+        print("Спасибо, что воспользовались нашим приложением")
