68b9e1b15faffec6b185bd286a031fac63f1ced7
Alexander Panferov <alexandr.n.panferov@gmail.com>
2023-08-20 21:04:41 +0700

    correct

--- src/classes.py
+++ src/classes.py
@@ -62,55 +62,54 @@ class HH(Platform):
         print(f"Отбор осуществляется из {len(self.vacancies_all)} вакансий (проверка обращения к сервису)")
         return self.vacancies_dicts
 
-    class SJ(Platform):
+
+class SJ(Platform):
+    """
+    Класс для работы с SJ
+    """
+    vacancies = []
+    vacancies_dicts = []
+    def __init__(self, vacancy):
+        self.vacancy = vacancy
+        self.api_key = os.getenv('SJ_API_KEY')
+    def get_request(self):
         """
-        Класс для работы с SJ
+        Отправляет запрос на SJ. записывает полученную информацию в файл
+        :return: список словарей с вакансиями
         """
-        vacancies = []
-        vacancies_dicts = []
+        url = 'https://api.superjob.ru/2.0/vacancies/'
+        headers = {'X-Api-App-Id': os.getenv('SJ_API_KEY')}
 
-        def __init__(self, vacancy):
-            self.vacancy = vacancy
-            self.api_key = os.getenv('SJ_API_KEY')
+        params = {
+            'keywords': f'{self.vacancy}',
+            'per_page': 100,
+            'area': 113,
+            'page': 0}
 
-        def get_request(self):
-            """
-            Отправляет запрос на SJ. записывает полученную информацию в файл
-            :return: список словарей с вакансиями
-            """
-            url = 'https://api.superjob.ru/2.0/vacancies/'
-            headers = {'X-Api-App-Id': os.getenv('SJ_API_KEY')}
+        while len(self.vacancies) <= 50:
+            response = requests.get(url, headers=headers, params=params)
+            response_decode = response.content.decode()
+            response.close()
+            data = json.loads(response_decode)
+            vacancies = data['objects']
+            for vacancy in vacancies:
+                if vacancy['payment_from'] != 0 and vacancy['payment_to'] != 0 and vacancy['currency'] == 'rub':
+                    try:
+                        self.vacancies.append(
+                            [vacancy['client']['title'], vacancy['profession'],
+                             vacancy['link'], vacancy['candidat'],
+                             vacancy['payment_from'], vacancy['payment_to']])
+                    except KeyError:
+                        continue
+            params['page'] += 1
+        for vacancy in self.vacancies:
+            vacancy_dict = {'employer': vacancy[0], 'name': vacancy[1], 'url': vacancy[2],
+                            'requirement': vacancy[3],
+                            'salary_from': vacancy[4], 'salary_to': vacancy[5]}
+            if vacancy_dict['salary_to'] == 0:
+                vacancy_dict['salary_to'] = vacancy_dict['salary_from']
+            self.vacancies_dicts.append(vacancy_dict)
+        with open(f'{self.vacancy}_sj.json', 'w', encoding='UTF-8') as file:
+            json.dump(self.vacancies_dicts, file, indent=2, ensure_ascii=False)
 
-            params = {
-                'keywords': f'{self.vacancy}',
-                'per_page': 100,
-                'area': 113,
-                'page': 0}
-
-            while len(self.vacancies) <= 50:
-                response = requests.get(url, headers=headers, params=params)
-                response_decode = response.content.decode()
-                response.close()
-                data = json.loads(response_decode)
-                vacancies = data['objects']
-                for vacancy in vacancies:
-                    if vacancy['payment_from'] != 0 and vacancy['payment_to'] != 0 and vacancy['currency'] == 'rub':
-                        try:
-                            self.vacancies.append(
-                                [vacancy['client']['title'], vacancy['profession'],
-                                 vacancy['link'], vacancy['candidat'],
-                                 vacancy['payment_from'], vacancy['payment_to']])
-                        except KeyError:
-                            continue
-                params['page'] += 1
-            for vacancy in self.vacancies:
-                vacancy_dict = {'employer': vacancy[0], 'name': vacancy[1], 'url': vacancy[2],
-                                'requirement': vacancy[3],
-                                'salary_from': vacancy[4], 'salary_to': vacancy[5]}
-                if vacancy_dict['salary_to'] == 0:
-                    vacancy_dict['salary_to'] = vacancy_dict['salary_from']
-                self.vacancies_dicts.append(vacancy_dict)
-            with open(f'{self.vacancy}_sj.json', 'w', encoding='UTF-8') as file:
-                json.dump(self.vacancies_dicts, file, indent=2, ensure_ascii=False)
-
-            return self.vacancies_dicts
+        return self.vacancies_dicts
