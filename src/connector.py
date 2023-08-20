e2b20e0aa7e036c02f915ecd100aa4aea229ace8
Alexander Panferov <alexandr.n.panferov@gmail.com>
2023-08-20 21:10:31 +0700

    add module SJ

--- src/connector.py
+++ src/connector.py
@@ -1,5 +1,5 @@
 import json
-from classes import HH
+from src.classes import HH, SJ
 
 
 class Connector:
@@ -32,3 +32,26 @@ class Connector:
             hh_info.append(vacancy)
 
         return hh_info
+
+    def connectSJ(self):
+        """
+        Проверка на существование файла от SJ с данными и
+        создание его при необходимости
+        """
+        try:
+            with open(f'{self.vacancy}_sj.json', encoding='utf-8') as file:
+                self.vacancies_sj = json.load(file)
+        except FileNotFoundError:
+            print("Файл не найден, создание нового файла")
+            self.vacancies_sj = SJ(self.vacancy).get_request()
+
+    def select_SJ(self):
+        """
+        Изменяет созданный файл для последующей его обработки
+        """
+        sj_info = []
+
+        for vacancy in self.vacancies_sj:
+            sj_info.append(vacancy)
+
+        return sj_info
